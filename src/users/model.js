1ae85da04ebc0a7cf58cb5aec9d4416b8dc83999
CatherineStone <106274539+CatherineStone@users.noreply.github.com>
2023-03-21 11:44:00 +0000

    user model

--- src/users/model.js
+++ src/users/model.js
@@ -1,4 +1,22 @@
-// const { DataTypes } = require("sequelize");
-// const connection = require("../db/connection");
+const { DataTypes } = require("sequelize");
+const connection = require("../db/connection");
 
-// const User = connection
+const User = connection.define("User", {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+},
+    { timestamps: false,
+        indexes: [{ unique: true, fields: ["email"] }] }
+);
+
+module.exports = User;
