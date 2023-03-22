0763312279b617bcc914717ef78146247e4513e9
Jessica Fisk <fisk.jess@gmail.com>
2023-03-22 11:07:49 +0000

    Added controllers addFavMeal and getAllFavMeals

--- src/Favourites/controller.js
+++ src/Favourites/controller.js
@@ -1,2 +1,47 @@
 
-getAllFavourites, addFavourite
+const Favourite = require("./model")
+const jwt = require("jsonwebtoken")
+
+
+
+const addFavourite = async (req, res) => {
+    try {
+        if (!req.authCheck) {
+            const error = new Error("User is not authorised");
+            res.status(401).json({ errorMessage: error.message, error: error });
+        }
+        const favourite = await Favourite.create(req.body);
+        res.status(201).json({ message: "success", favourite: favourite });
+    }
+    catch (error) {
+        res.status(501).json({ errorMessage: error.message, error: error });
+    }
+}
+
+
+
+
+
+const getAllFavourites = async (req, res) => {
+    try {
+        const favMeals = await Favourite.findAll({where: { user: req.params.user },
+        include: Favourite,
+    });
+        res.status(200).json({ message: "success", books: favMeals });
+    }
+    catch (error) {
+        res.status(501).json({ errorMessage: error.message, error: error });
+    }
+}
+
+
+
+
+
+
+
+
+module.exports = {
+    getAllFavourites, 
+    addFavourite,
+}
